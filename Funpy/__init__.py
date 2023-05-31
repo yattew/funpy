d59597d40b3f178bcd505ae821f5bd2118addf29
yattew <yathansh@gmail.com>
2023-06-01 00:25:01 +0530

    simplified the whole operation

--- Funpy/__init__.py
+++ Funpy/__init__.py
@@ -1,24 +1,23 @@
-from typing import Any, TypeVar, Callable
+from typing import Any, TypeVar, Callable, Union
 T = TypeVar('T')
 
 
 
 # adding a pipe operator in python
-# using the >> to display the piping
+# using the | to display the piping
+class ApplicationChain:
+    pass
+
 
 class FunctionalContainer:
     def __init__(self, val:T) -> None:
         self.val = val
 
-    def __or__(self, other: Callable) -> "FunctionalContainer":
+    def __or__(self, other: Union[Callable, ApplicationChain]) -> "FunctionalContainer":
         res = other(self.val)
-        if other.keep == True:
-            self.val = res
-            return self
         return FunctionalContainer(res)
 
-    def __call__(self) -> T:
+    def __call__(self, other = None) -> T:
+        if other:
+            return self.val(other)
         return self.val
-
-class ApplicationChain:
-    pass
--- main.py
+++ main.py
@@ -1,19 +1,14 @@
 from Funpy import FunctionalContainer as FC
-from Funpy import functions as F
 
-a = FC(10)
+l = FC([1, 2, 3, 4, 5])
 
-(
-    a
-    | F.sq()
-    | F.print()
-)
 
-a | F.print()
+def fmap(fn) -> FC:
+    return FC(lambda x: list(map(fn, x)))
 
 
 (
-    a
-    | F.sq(keep=True)
-    | F.print()
+    l
+    | fmap(lambda x: x**2)
+    | FC(print)
 )
